32b00936afef1716ff1e59000e97ef36cbd2f88f
Henry <henry@gedert.de>
2024-02-03 22:54:15 +0100

    added "s"

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: 'index.html',
-			base: process.env.NODE_ENV === 'production' ? '/ani-season-chart' : '',
+			base: process.env.NODE_ENV === 'production' ? '/ani-season-charts/' : '',
 		})
 	}
 };
